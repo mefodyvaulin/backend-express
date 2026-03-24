e3214135a57e9ca021e5f74aa5d7026d896b430d
Mefody_Vaulin <181823089+mefodyvaulin@users.noreply.github.com>
2026-03-24 17:20:24 +0500

    complete 4-6

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const router = express.Router();
 
+const users = [{'id' : 1, 'name': 'Кристина'}, {'id' : 2, 'name': 'Мефодий'}]
+const globalId = 0
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Hello, world!');
+  res.send({'item' : users});
 });
+
+router.post('/', function(req, res, next) {
+    let newId = users.at(-1).id
+    newId += 1
+    const newUser = {'id': newId, 'name': req.body.name};
+    users.push(newUser);
+    res.status(201).json(newUser);
+})
+
+router.get('/:id', function(req, res, next) {
+    let id = +req.params.id;
+    const currentUser = users.find(user => user.id == id)
+    if (!currentUser) {
+        res.status(404).send('Not Found');
+    }
+    res.send(currentUser);
+})
 
 module.exports = router;
