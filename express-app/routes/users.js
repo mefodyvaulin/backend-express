17ba365ea18aa2a472d645d973f6fd713f81f441
Mefody_Vaulin <181823089+mefodyvaulin@users.noreply.github.com>
2026-03-24 18:06:26 +0500

    complete 8

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -6,27 +6,37 @@ db.run(`CREATE TABLE IF NOT EXISTS users (
    id INTEGER PRIMARY KEY AUTOINCREMENT,
    name text)`);
 
-const users = [{'id' : 1, 'name': 'Кристина'}, {'id' : 2, 'name': 'Мефодий'}]
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send({'item' : users});
+    db.all("SELECT id, name FROM users", [], (err, rows) => {
+        if (err) {
+            next(err);
+        } else {
+            res.json(rows);
+        }
+    });
 });
 
 router.post('/', function(req, res, next) {
-    let newId = users.at(-1).id
-    newId += 1
-    const newUser = {'id': newId, 'name': req.body.name};
-    users.push(newUser);
-    res.status(201).json(newUser);
+    const newUserName = req.body.name;
+    const insert = "INSERT INTO users (name) VALUES (?)";
+    db.run(insert, [newUserName]);
+    return {"id": newUserName};
 })
 
 router.get('/:id', function(req, res, next) {
-    let id = +req.params.id;
-    const currentUser = users.find(user => user.id == id)
-    if (!currentUser) {
-        res.status(404).send('Not Found');
-    }
-    res.send(currentUser);
+
+    db.all("SELECT id, name FROM users WHERE id = (?)", [+req.params.id], (err, rows) => {
+        if (err) {
+            next(err);
+        } else {
+            if (rows.length === 0) {
+                res.status(404).send("No records found");
+            }
+            res.json(rows);
+        }
+    });
 })
 
 module.exports = router;
