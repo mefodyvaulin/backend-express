5a0fc54e94158ee4cea884be006ee38afe680d6a
Applopie <stina.nekrasova.2@yandex.ru>
2026-03-24 17:10:36 +0500

    Changed users to team members

--- express-app/routes/users.js
+++ express-app/routes/users.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('Hello, world!');
+  res.send({items: [{"id": 1, "name":"Мефодий"}, {"id": 2, "name": "Кристина"}]});
 });
 
 module.exports = router;
